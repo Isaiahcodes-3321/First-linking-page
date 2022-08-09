1d18cdeebeb86faef90f439e1cbdcab27cf6f2eb
isaiah <106246256+Isaiahcodes-3321@users.noreply.github.com>
2022-08-09 22:46:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// const menuBtn = document.querySelector('.fa-bars')
-// const navlink = document.querySelector('.nav-link')
-
-// menuBtn.addEventListener('click',()=>{
-//        navlink.classList.toggle('mobile-menu')
-//    })
