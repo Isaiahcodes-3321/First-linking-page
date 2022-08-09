c93c0ad43565c45f5623055bcc5fb5463e8789d0
isaiah <106246256+Isaiahcodes-3321@users.noreply.github.com>
2022-08-09 22:50:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// const menuBtn = document.querySelector('.fa-bars')
+// const navlink = document.querySelector('.nav-link')
+
+// menuBtn.addEventListener('click',()=>{
+//        navlink.classList.toggle('mobile-menu')
+//    })
